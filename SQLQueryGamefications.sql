8dc8e0af4dfbbde7b0d47cc14678e6bc2b1e82a1
Messering <messering@yandex.ru>
2016-03-11 04:09:22 +0200

    Change sql query create db- add autoincrement and add users column email, image

--- SQLQueryGamefications.sql
+++ SQLQueryGamefications.sql
@@ -1,5 +1,5 @@
 create table Users(
-id_user int NOT NULL,
+id_user int IDENTITY(1,1) NOT NULL,
 name varchar(50) NOT NULL,
 nickname varchar(50) NOT NULL,
 passwords varchar(35) NOT NULL,
@@ -7,7 +7,7 @@ primary key (id_user)
 )
 
 create table Ranks(
-id_rank int NOT NULL,
+id_rank int IDENTITY(1,1) NOT NULL ,
 title varchar(50) NOT NULL,
 Images varchar(50) NOT NULL,
 points int,
@@ -16,7 +16,7 @@ primary key (id_rank)
 )
 
 create table Badges(
-id_badge int NOT NULL,
+id_badge int IDENTITY(1,1) NOT NULL ,
 title varchar(50) NOT NULL,
 Images varchar(50) NOT NULL,
 points int NOT NULL,
@@ -26,7 +26,7 @@ primary key (id_badge)
 )
 
 create table Levels(
-id_level int NOT NULL,
+id_level int IDENTITY(1,1) NOT NULL ,
 title varchar(50) NOT NULL,
 "rank" int NOT NULL,
 points int NOT NULL,
@@ -35,7 +35,7 @@ FOREIGN KEY ("rank") REFERENCES Ranks(id_rank)
 )
 
 create table Activity(
-id_activity int NOT NULL,
+id_activity int IDENTITY(1,1) NOT NULL ,
 title varchar(50) NOT NULL,
 content varchar(50) NOT NULL,
 Images varchar(50) NOT NULL,
@@ -45,14 +45,14 @@ FOREIGN KEY (id_user) REFERENCES Users(id_user)
 )
 
 create table Helpers(
-id_helper int NOT NULL,
+id_helper int IDENTITY(1,1) NOT NULL ,
 title varchar(50) NOT NULL,
 "message" varchar(50),
 primary key (id_helper)
 )
 
 create table Prizes(
-id_prize int NOT NULL,
+id_prize int IDENTITY(1,1) NOT NULL ,
 name_prize varchar(50) NOT NULL,
 Images varchar(50) NOT NULL,
 points int,
@@ -60,7 +60,7 @@ primary key (id_prize)
 )
 
 create table History(
-id_history int NOT NULL,
+id_history int IDENTITY(1,1) NOT NULL ,
 id_user int NOT NULL,
 points int NOT NULL,
 record_date Date NOT NULL,
@@ -70,7 +70,7 @@ FOREIGN KEY (id_level) REFERENCES Levels(id_level)
 )
 
 create table Profiles(
-id int NOT NULL,
+id int IDENTITY(1,1) NOT NULL ,
 id_user int NOT NULL,
 points int NOT NULL,
 "level" int NOT NULL,
@@ -81,3 +81,6 @@ FOREIGN KEY (id_user) REFERENCES Users(id_user),
 FOREIGN KEY ("level") REFERENCES Levels(id_level),
 FOREIGN KEY ("rank") REFERENCES Ranks(id_rank)
 )
+
+alter table dbo.Users add email varchar(30)
+alter table dbo.Users add "Image" varchar(50)
